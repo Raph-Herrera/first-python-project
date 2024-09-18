f2bad9a0e7a156221fa0c1f4205178c80e13555e
Raph-Herrera <silanerrera10@gmail.com>
2024-09-18 14:38:09 +0800

    First Upload

---
+++ hello.py
@@ -0,0 +1,9 @@
+'''
+print Hello World
+Print Firstname, Lastname in a variable
+Display your Fullname "Hello, my name is Fullname"
+'''
+first_name = "Raphael"
+last_name = "Herrera"
+full_name = first_name + last_name
+print("Hello, my name is " + full_name)
